1fde844f371a6ca793eabbdfa7db3f2f2e09194c
Alex Clarke <aclarke211@gmail.com>
2021-02-27 20:54:12 +0000

    chore: :fire: remove unused styles.

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,4 @@ export default {
     HeaderComp,
   },
 };
-
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>

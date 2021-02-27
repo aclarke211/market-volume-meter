d1716b189e346526fe772b18fe3862b26f480398
Alex Clarke <aclarke211@gmail.com>
2021-02-27 01:40:42 +0000

    feat: :sparkles: values for volume statistics.
    Default values added for volume stats for each stock item.

--- src/components/StockItem.vue
+++ src/components/StockItem.vue
@@ -1,8 +1,28 @@
 <template>
   <div :class="className">
-    <div :class="`${className}__name`">
+    <div :class="`${className}__name-container`">
       <h3 :class="`${className}__text`">{{ name.text }}</h3>
       <h4 :class="`${className}__symbol`">{{ name.symbol }}</h4>
+    </div>
+
+    <div class="percent__container">
+      <div class="percent__title">Percent</div>
+      <div class="percent__value">{{ percentValue }}%</div>
+    </div>
+
+    <div class="volume__container">
+      <div class="volume-item relative">
+        <div class="volume-item__name">Relative Vol.</div>
+        {{ volume.relative }}
+      </div>
+      <div class="volume-item total">
+        <div class="volume-item__name">Total Vol.</div>
+        {{ volume.total }}
+      </div>
+      <div class="volume-item current">
+        <div class="volume-item__name">Current Vol.</div>
+        {{ volume.current }}
+      </div>
     </div>
   </div>
 </template>
@@ -19,44 +39,80 @@ export default {
         symbol: 'SYMB',
       }),
     },
+
+    volume: {
+      type: Object,
+      default: () => ({
+        relative: 14.4,
+        total: 40.25,
+        current: 20.11,
+      }),
+    },
   },
 
   data: () => ({
     className: 'stock-item',
   }),
+
+  computed: {
+    percentValue() {
+      return 58;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
 /* Sass Variables */
-$className: '.stock-item';
+$className: ".stock-item";
 
 #{$className} {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+
+  &__name-container {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
 
-    &__name {
+  }
+
+  &__text {
+    font-size: 1.25rem;
+    font-weight: 400;
+  }
+
+  &__symbol {
+    color: darkgrey;
+    font-size: 0.75rem;
+    font-weight: 600;
+    letter-spacing: 0.1rem;
+    margin: 0.25rem;
+  }
+
+  .percent__container {
+    margin: .25rem;
+  }
+
+  .volume__container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin: .25rem;
+
+    .volume-item {
       display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
+      flex-direction: column;
       justify-content: center;
       align-items: center;
-    }
-
-    &__text {
-      font-size: 1.25rem;
-      font-weight: 400;
-    }
-
-    &__symbol {
-      color: darkgrey;
-      font-size: .75rem;
-      font-weight: 600;
-      letter-spacing: 0.1rem;
       margin: .25rem;
     }
   }
+}
 </style>

72910db36ca8f17172c892bc6f9821c2553fda27
Alex Clarke <aclarke211@gmail.com>
2021-02-28 21:05:51 +0000

    feat: :wheelchair: ability to switch between clock formats.
    Can show clocks in either 12-hour or 24-hour formats.

--- src/utils/date-manager.js
+++ src/utils/date-manager.js
@@ -6,15 +6,15 @@ function toUTCDate(date) {
   return new Date(date).toUTCString();
 }
 
-function getLocalDate(utcString) {
+function getLocalDate(utcString, ampm = true) {
   if (utcString) {
-    return new Date(utcString).toLocaleString('en-US', Intl.DateTimeFormat().resolvedOptions().timeZone);
+    return new Date(utcString).toLocaleString(ampm ? 'en-US' : 'en-GB', Intl.DateTimeFormat().resolvedOptions().timeZone);
   }
-  return new Date().toLocaleString('en-US', Intl.DateTimeFormat().resolvedOptions().timeZone);
+  return new Date().toLocaleString(ampm ? 'en-US' : 'en-GB', Intl.DateTimeFormat().resolvedOptions().timeZone);
 }
 
-function getTimezoneDate(location = 'America/Los_Angeles') {
-  return new Date().toLocaleString('en-US', { timeZone: location });
+function getTimezoneDate(location = 'America/Los_Angeles', ampm = true) {
+  return new Date().toLocaleString(ampm ? 'en-US' : 'en-GB', { timeZone: location });
 }
 
 module.exports = {
